63458481a362e26d4b5525d5a6210d9a30a2b433
Ryan Richholt <rrichholt@tgen.org>
2017-12-18 18:30:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Game
+
+Front end react application including: landing page, user profile pages, create/edit user 
+profile pages, and leader board.
---
+++ src/pages/Leaderboard/leaderboard.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+
+class Leaderboard extends Component {
+  state = {
+    name: [],
+    email: "",
+    score: "",
+    date: ""
+  };
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  loadUsers = () => {
+    API.getUsers()
+      .then(res =>
+        this.setState({ users: res.data, name: "", email: "", score: "", date: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Leaderboard?</h1>
+            </Jumbotron>
+
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Leaderboard;
